bcea0e77aefbb7c2e4e3f52160dbdfe009f5acd2
Yuliia Mikhno <ryabukha.yulia@gmail.com>
2025-08-18 20:42:41 +0200

    Загружает контакты из файла contacts.txt, если файл существует и данные корректны.

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import S
-S
+import txt
+import os
+import re
+import sys
+
+CONTACTS_FILE = 'contacts.txt'
+
+current_path = sys.path
+print(current_path)
+
+file_path = txt
+sys.path.append(file_path)
